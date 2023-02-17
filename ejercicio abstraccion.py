06803bde765f2771b220b921b44a6706d2a05804
EstebanAJ1 <esteban.alvarezj@udea.edu.co>
2023-02-17 11:50:31 -0500

    2do ejercicio abstraccion

--- ejercicio abstraccion.py
+++ ejercicio abstraccion.py
@@ -24,8 +24,13 @@ class Persona():
     def verGenero(self):
         return self.__genero
 
+    def guardarInfo(self):
+        return self.__nombre, self.__cedula, self.__genero
+
 class Paciente(Persona):
     def __init__(self):
+        super().__init__()
+        # Persona.__init__(self)
         self.__servicio = ""
     
     def asignarServicio(self, rol):
@@ -67,10 +72,14 @@ class Medico(Empleado_Hospital):
 class Sistema(Persona):
     def __init__(self):
         self.__lista_pacientes = []
-        
+        self.__lista_nombre = []
+        self.__lista_cedula = []
+        self.__lista_genero = []
+        self.__diccionario_paciente = {}
+
     def numPacientes(self):
         self.__numero_pacientes = len(self.__lista_pacientes)
-        return self.__numero_pacientes
+        return print(self.__numero_pacientes)
 
     def ingresarPaciente(self, rol):
         p = Paciente()
@@ -78,10 +87,49 @@ class Sistema(Persona):
         p.asignarCedula(rol)
         p.asignarGenero(rol)
         p.asignarServicio(rol)
-        self.__lista_pacientes.append(p)
-        print(self.numPacientes())
+        self.__lista_pacientes.append(p.guardarInfo())
+        self.__lista_nombre.append(p.verNombre())
+        self.__lista_cedula.append(p.verCedula())
+        self.__lista_genero.append(p.verGenero())
+        self.__diccionario_paciente.update({"Nombre": self.__lista_nombre, "Cedula": self.__lista_cedula, "Genero": self.__lista_genero})
+        #print(self.__lista_pacientes)
+        #print(self.__diccionario_pacientes)
+        #print(self.numPacientes())
 
+    def verDatosPacientesLista(self):
+        cedula = str(input("Ingresar la cedula del paciente que busca en la lista: "))
+        for c in self.__lista_pacientes:
+            if cedula == c[1]:
+                return print(c)
 
-p1 = Sistema()
-p1.ingresarPaciente("Paciente")
+    def verDatosPacientesDiccionario(self):
+        cedula = (input("Ingresar la cedula del paciente que busca en el diccionario: "))
+        for p, c in enumerate(self.__diccionario_paciente["Cedula"]):
+            if cedula == c:
+                print(f"Nombre: {self.__diccionario_paciente['Nombre'][p]} ")
 
+def main():
+    p = Sistema()
+
+    while True:
+        opcion = int(input('''
+        - 1. Ingresar paciente 
+        - 2. Ver datos del paciente
+        - 3. ver el numero de pacientes
+        - 4. Salir
+        > '''))
+        if opcion == 1:
+            p.ingresarPaciente("Paciente")
+
+        elif opcion == 2:
+            p.verDatosPacientesDiccionario()
+
+        elif opcion == 3:
+            p.numPacientes()
+
+        elif opcion == 4:
+            break
+
+# main()
+if __name__ == '__main__':
+    main()
