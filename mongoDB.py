0a9759d47bd756f149090c7c2b96f649e8af86c0
EstebanAJ1 <esteban.alvarezj@udea.edu.co>
2023-02-24 11:50:40 -0500

    mongoDB

---
+++ mongoDB.py
@@ -0,0 +1,26 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://EstebanAJ1:esteban123@cluster0.6v6oixp.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+mydb = client["bbdd"] # Base de datos
+mycol = mydb["clientes"] # Coleccion
+
+mydict = {"nombre": "Esteban", "direccion": "C/ Mayor 1"}
+x =mycol.insert_one(mydict) 
+
+print(x.inserted_id)
+
+myquery = {"nombre": "Esteban", "direccion": "C/ Mayor 1" }
+newvalues = {"$set": {"nombre":"Luis"}}
+
+mycol.update_one(myquery, newvalues)
+
+#Buscar los datos en la base de datos
+for y in mycol.find(): 
+    print(y)
+    
+# Buscar un dato en especifico de la base de datos
+for x in mycol.find({"nombre": "Luis"}): 
+    print(x)
+    #print(x["nombre"])
